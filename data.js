8999452967dd22e740961294159026daba91184a
StefanHochleithner <stefan.hochleithner@gmail.com>
2025-07-29 17:21:17 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const players = [
+const allPlayers = [
   "René", "Sascha", "Tom", "Laith", "Jan", "Willi", "Peter", "Josef", "Stefan", "Patrick T", "Werner", "Nico", "Dili", "Dominic", "Rudi", "Didi", "Chrisem", "Franz", "Roman", 
   "Michael K.", "Christoph S.", "Dustin", "Michael G.", "Patrick M.", "Ugur", "Jürgen", "Daniel K.", "Savas", "Gotti", "Valerian", "Rettich", "Esad", "Miguel"   
 ];
